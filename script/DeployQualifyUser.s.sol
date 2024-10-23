6a04135f1b4b0569bcf576848c6f5673308c45a0
apmfree78 <amit@profitswami.com>
2024-10-22 20:59:56 -0400

    fix: fix deploy changes

--- script/DeployQualifyUser.s.sol
+++ script/DeployQualifyUser.s.sol
@@ -13,14 +13,8 @@ import {QualifyUser} from "src/QualifyUser.sol";
 contract DeployQualifyUser is Script {
     function run() external returns (QualifyUser, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
-        // (address poolAddress, address dataProviderAddress, address priceOracleAddress,,,,) =
-        //     helperConfig.activeNetworkConfig();
-        // HelperConfig.NetworkConfig memory config = helperConfig.activeNetworkConfig();
-        // Assign struct fields to local variables
-        address poolAddress = helperConfig.poolAddress;
-        address dataProviderAddress = helperConfig.dataProviderAddress;
-        address priceOracleAddress = helperConfig.priceOracleAddress;
-        address[] memory pricedInEth = helperConfig.pricedInEth;
+        (address poolAddress, address dataProviderAddress, address priceOracleAddress,,,,) =
+            helperConfig.activeNetworkConfig();
 
         vm.startBroadcast();
         QualifyUser qualifyUser = new QualifyUser(poolAddress, dataProviderAddress, priceOracleAddress);
